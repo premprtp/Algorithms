ef61b0300a0b173eec540e67d2c27584083145a0
Prem Pratap <theprempratap@gmail.com>
2020-05-27 10:54:56 +0530

    Simple Array Sum sol

---
+++ Warmup/2.Simple Array Sum.py
@@ -0,0 +1,25 @@
+#Simple Array Sum | [Problem]https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#!/bin/python3
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    su=0
+    for i in range(ar_count):
+        su+=ar[i]
+    return su
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
