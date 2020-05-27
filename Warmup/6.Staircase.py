d966a867189bc74e159516bf3b40023bdedd80f0
Prem Pratap <theprempratap@gmail.com>
2020-05-27 11:44:16 +0530

    Staircase sol

---
+++ Warmup/6.Staircase.py
@@ -0,0 +1,17 @@
+#Plus Minus | [Problem](https://www.hackerrank.com/challenges/staircase/problem)
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def staircase(n):
+    for i in range(1,n+1):
+        print(" "* (n-i)+ "#" *i)
+
+if __name__ == '__main__':
+    n = int(input())
+    staircase(n)
