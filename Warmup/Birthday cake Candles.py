a00b1fbe59072d4807a8d6ae70591717b4e8b6cb
Prem Pratap <theprempratap@gmail.com>
2020-05-27 12:34:15 +0530

    Birthday Cake Candles sol

---
+++ Warmup/Birthday cake Candles.py
@@ -0,0 +1,30 @@
+#Birthday Cake Candles | [Problem](https://www.hackerrank.com/challenges/birthday-cake-candles/problem)
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(ar):
+    m=max(ar)
+    count=0
+    for i in ar:
+        if (i==m):
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
